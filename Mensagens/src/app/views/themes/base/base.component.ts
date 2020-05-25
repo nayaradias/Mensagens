380d3dc478133f3d489f5de6b72c32f55e13fd8e
nayaradias <nayaradias@users.noreply.github.com>
2020-05-24 22:13:53 -0300

    add color tema

--- Mensagens/src/app/views/themes/base/base.component.ts
+++ Mensagens/src/app/views/themes/base/base.component.ts
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
 import { MensagemService } from 'src/app/core/services/mensagens.service';
 import { MensagemI } from 'src/app/core/interfaces/mensagem.interface';
 import { Mensagem } from '../../pages/mensagens/mensagens.model';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-base',
@@ -18,14 +19,49 @@ export class BaseComponent implements OnInit {
     private autenticacao: AutenticacaoService,
     private router: Router
   ) {}
+  //bg-primaria:#343a40
+  //bg-secundaria:#fff
+  background: string = '#343a40';
+  color: string = '#ffffff';
   usuario: UsuarioI;
   mensagem: MensagemI;
   formMensagem: FormGroup;
   env = environment.apifile;
+  tema: any;
   ngOnInit(): void {
+    this.tema = localStorage.getItem('tema');
+    if (this.tema == '' || this.tema == undefined || this.tema == null) {
+      localStorage.setItem(
+        'tema',
+        JSON.stringify({ background: this.background, color: this.color })
+      );
+    } else {
+      this.background = this.tema.background;
+      this.color = this.tema.color;
+    }
+
     this.autenticacao.usuarioLogado().subscribe((usuario) => {
       this.usuario = usuario.usuario;
     });
+  }
+  SliderValue(event) {
+    if (event.target.value == 1) {
+      this.background = '#343a40';
+      this.color = '#cccccc';
+      localStorage.setItem(
+        'tema',
+        JSON.stringify({ background: this.background, color: this.color })
+      );
+    } else {
+      this.background = '#ffffff';
+      this.color = '#343a40';
+      localStorage.setItem(
+        'tema',
+        JSON.stringify({ background: this.background, color: this.color })
+      );
+    }
+
+    console.log(event.target.value);
   }
   sair() {
     localStorage.removeItem('token');
