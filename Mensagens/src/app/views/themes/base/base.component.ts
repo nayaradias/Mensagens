ce2c977f30b80177380330d5809674f3fcdc43ae
nayaradias <nayaradias@users.noreply.github.com>
2020-05-28 20:25:16 -0300

    add new upload

--- Mensagens/src/app/views/themes/base/base.component.ts
+++ Mensagens/src/app/views/themes/base/base.component.ts
@@ -10,6 +10,8 @@ import { Mensagem } from '../../pages/mensagens/mensagens.model';
 import { element } from 'protractor';
 import Swal from 'sweetalert2/dist/sweetalert2.js';
 import 'sweetalert2/src/sweetalert2.scss';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-base',
   templateUrl: './base.component.html',
@@ -18,7 +20,8 @@ import 'sweetalert2/src/sweetalert2.scss';
 export class BaseComponent implements OnInit {
   constructor(
     private autenticacao: AutenticacaoService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder,
   ) {}
   //bg-primaria:#343a40
   //bg-secundaria:#fff
@@ -27,7 +30,9 @@ export class BaseComponent implements OnInit {
   usuario: UsuarioI;
   mensagem: MensagemI;
   formMensagem: FormGroup;
+  uploadForm: FormGroup;
   env = environment.apifile;
+  api = environment.api;
   tema = {
     background: '#343a40',
     color: '#343a40',
@@ -44,7 +49,12 @@ export class BaseComponent implements OnInit {
     }
 
     this.autenticacao.usuarioLogado().subscribe((usuario) => {
+
       this.usuario = usuario.usuario;
+      console.log('Usuario', this.usuario);
+    });
+    this.uploadForm = this.formBuilder.group({
+      profile: [''],
     });
   }
   SliderValue(event) {
@@ -64,27 +74,51 @@ export class BaseComponent implements OnInit {
     return thema;
     // console.log(event.target.value);
   }
-  async upload() {
-    const { value: file } = await Swal.fire({
-      title: 'Selecione uma imagem',
-      input: 'file',
-      inputAttributes: {
-        accept: 'image/*',
-        'aria-label': 'Upload',
-      },
-    });
+  // async upload() {
+  //   const { value: file } = await Swal.fire({
+  //     title: 'Selecione uma imagem',
+  //     input: 'file',
+  //     inputAttributes: {
+  //       accept: 'image/*',
+  //       'aria-label': 'Upload',
+  //     },
+  //   });
 
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        Swal.fire({
-          title: 'Imagem adicionada com sucesso',
-          imageUrl: e.target.result,
-          imageAlt: 'Imagem',
-        });
-      };
-      reader.readAsDataURL(file);
+  //   if (file) {
+  //     const reader = new FileReader();
+  //     reader.onload = (e) => {
+  //       Swal.fire({
+  //         title: 'Imagem adicionada com sucesso',
+  //         imageUrl: e.target.result,
+  //         imageAlt: 'Imagem',
+  //       });
+  //     };
+  //     reader.readAsDataURL(file);
+  //     console.log('file:',file);
+  //     this.autenticacao.adicionarFoto(file.name).subscribe(res => {
+  //       console.log('upload Foto:',res);
+  //     });
+  //   }
+  // }
+  onFileSelect(event) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.uploadForm.get('profile').setValue(file);
     }
+  }
+  onSubmit() {
+    // const formData = new FormData();
+    // formData.append('imagem', this.uploadForm.get('profile').value);
+    // console.log('formData:',formData);
+    // console.log('formData:',this.uploadForm.get('profile').value);
+    var formData: FormData = new FormData();
+    console.log(formData);
+    formData.append('image',this.uploadForm.get("profile").value);
+    console.log(formData);
+    this.autenticacao.adicionarFoto(formData).subscribe(
+      (res) => console.log(res),
+      (err) => console.log(err)
+    );
   }
   sair() {
     localStorage.removeItem('token');
